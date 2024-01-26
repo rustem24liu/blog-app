0e046e0cbe420e86c58408dea108b908f47a545b
rustem24liu <temirgalirustem05@gmail.com>
2024-01-26 17:24:37 +0600

    something in the way

---
+++ personal_blog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+
+posts_list = [
+    {
+        'pk':1,
+        'title':'rustem',
+        'description':'post description1',
+        'body':'lorem ipsum dolor sit',
+        'image_url':'https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg'
+    },
+{
+'pk':2,
+        'title':'I love',
+        'description':'post description2',
+        'body':'lorem ipsum dolor sit',
+        'image_url':'https://cdn.britannica.com/99/197999-050-D22B29F0/Leopard-cat.jpg'
+    },
+{
+'pk':4,
+        'title':'I just died in your arms to night',
+        'description':'post description3',
+        'body':'lorem ipsum dolor sit',
+        'image_url':'https://cdn.britannica.com/25/7125-050-67ACEC3C/Abyssinian-sorrel.jpg'
+    },
+{
+'pk':5,
+        'title':'How I wish, how i wish you were here',
+        'description':'post description4',
+        'body':'lorem ipsum dolor sit',
+        'image_url':'https://cdn.britannica.com/53/155253-050-642F3524/Egyptian-cat-statue-Bastet.jpg'
+    },
+{
+'pk':6,
+        'title':'I will see you on the dark side of the moon',
+        'description':'post description5',
+        'body':'lorem ipsum dolor sit',
+        'image_url':'https://cdn.britannica.com/62/5162-004-E7F758CE/Dick-Whittington-portrait-cat-engraving-Renold-Elstracke.jpg?w=300'
+    }
+]
+def index_view(request):
+    return render(request, 'index.html', context={
+        'posts': posts_list, 'user_name': 'Rustem Temirgali'
+    })
+
+def post_view(request, pk):
+    post = {}
+    for post in posts_list:
+        if post.get('pk') == pk:
+            post = post
+    return render(request, 'post_view.html',
+                  context={'post':post
+    })
